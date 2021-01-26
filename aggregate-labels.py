d1f8f47464b25e3e00aa304a6e09106cef6baef4
Sergiy Matusevych <sergiym@microsoft.com>
2021-01-26 13:40:37 -0800

    more pythonic version of label aggregation

--- aggregate-labels.py
+++ aggregate-labels.py
@@ -17,22 +17,17 @@ labels_noise = labels_all - (labels_music | labels_speech)
 
 data = pandas.read_csv("mobileNet_trainingdata_V1_labels_Wavegram_Logmel_Cnn14.csv.bz2")
 
-index_music = []
-index_speech = []
-index_noise = []
-for (i, label) in enumerate(data.columns):
-    if label in labels_music:
-        index_music.append(label)
-    elif label in labels_speech:
-        index_speech.append(label)
-    elif label in labels_noise:
-        index_noise.append(label)
+# make sure we use only the available labels
+labels_available = frozenset(data.columns)
+labels_music &= labels_available
+labels_speech &= labels_available
+labels_noise &= labels_available
 
 res = pandas.DataFrame({
     "filename": data.filename,
-    "music": data[index_music].sum(axis=1),
-    "speech": data[index_speech].sum(axis=1),
-    "noise": data[index_noise].sum(axis=1)
+    "music": data[labels_music].sum(axis=1),
+    "speech": data[labels_speech].sum(axis=1),
+    "noise": data[labels_noise].sum(axis=1)
 })
 
 res.to_csv("mobileNet_trainingdata_V1_labels_3way.csv.bz2",
